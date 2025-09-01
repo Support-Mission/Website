c0b038865c9843d829e60ae46069d37a73ffa97c
buggzycalhoun <christopher.bugg@supportmission.services>
2025-08-31 21:38:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const toggle = document.querySelector('.menu-toggle');
-  const links = document.querySelector('.nav-links');
-  if (toggle && links) {
-    toggle.addEventListener('click', () => links.classList.toggle('open'));
-  }
-})();
