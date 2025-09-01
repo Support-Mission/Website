91b74d2cde547f3a2f0eb87b6b8ae3ba3c4dfaa1
buggzycalhoun <christopher.bugg@supportmission.services>
2025-08-31 21:25:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+(function(){
+  const toggle = document.querySelector('.menu-toggle');
+  const links = document.querySelector('.nav-links');
+  if (toggle && links) {
+    toggle.addEventListener('click', () => links.classList.toggle('open'));
+  }
+})();
